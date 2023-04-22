eafe4be245579049bec70520c34f5bfa7251bf5a
Margotte83 <marjorie.ngoupende@gmail.com>
2023-04-22 16:15:10 +0200

    ✨ F01: LOGIN > add title & form

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useState } from "react";
 
 export default function Login() {
+  //state
+  const [value, setValue] = useState("");
+  // comportements
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Bonjour ${value}`);
+  };
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  //affichage
   return (
     <div>
-      Login page
+      <h1>Bienvenue chez nous</h1>
+      <br/>
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Entrez votre prénom..."
+          required
+          value={value}
+          onChange={handleChange}
+        />
+
+        <button type="submit">Accéder à votre espace</button>
+      </form>
     </div>
-  )
+  );
 }
