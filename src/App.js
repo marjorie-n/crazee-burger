5a9b8bbae6ea7ffbaa027b6ef046d5b580b8695c
Margotte83 <marjorie.ngoupende@gmail.com>
2023-04-27 14:46:07 +0200

    ✨ F02: APP> install routing system

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import Login from "./components/pages/login/Login";
-
+import OrderPage from "./components/pages/order/OrderPage";
+import ErrorPage from "./components/pages/error/ErrorPage";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/order" element={<OrderPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/pages/error/ErrorPage.js
@@ -0,0 +1,8 @@
+export default function ErrorPage() {
+  return (
+    <div>
+      <h1>Error Page</h1>
+      <button>Retounez à la page d'accueil</button>
+    </div>
+  );
+}
