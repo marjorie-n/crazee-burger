3d6eb776ecf5c269d342e5ffc5a1af47654eac4c
Margotte83 <marjorie.ngoupende@gmail.com>
2023-05-11 14:57:59 +0200

    💄 F03: LOGO > add styling

--- src/components/reusable-UI/Logo.js
+++ src/components/reusable-UI/Logo.js
@@ -3,11 +3,35 @@ import styled from "styled-components";
 export default function Logo() {
   return (
     <LogoStyled>
-      LOGO CRAZEE BURGER
+      <h1>CRAZEE</h1>
+      <img src="/images/ logo-orange.png" alt="logo burger" />
+      <h1>BURGER</h1>
     </LogoStyled>
-  )
-} 
-const LogoStyled  = styled.div`
-  background-color: blue;
-  
+  );
+}
+const LogoStyled = styled.div`
+  border: 1px solid blue;
+  display: flex;
+  align-items: center;
+  transform: scale(2.5);
+
+  h1 {
+    display: inline;
+    text-align: center;
+    color: #f5a623;
+    font-size: 36px;
+    line-height: 1rem;
+    font-weight: 700;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    font-family: "Amatic SC", cursive;
+  }
+
+  img {
+    object-fit: contain;
+    object-position: center;
+    height: 60px;
+    width: 80px;
+    margin: 0 5px;
+  }
 `;
