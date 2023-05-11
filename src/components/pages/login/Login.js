274bfe1138928aeabbec449cb30aa5e28752f34e
Margotte83 <marjorie.ngoupende@gmail.com>
2023-05-11 14:27:58 +0200

    💄 F03:LOGIN > add styling & background

--- src/components/pages/login/Login.js
+++ src/components/pages/login/Login.js
@@ -12,10 +12,27 @@ export default function Login() {
   );
 }
 const LoginStyled = styled.div`
-  background-color: red;
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+
+  ::before {
+    content: "";
+    background: url("/images/burger-background.jpg");
+    background-color: rgba(0, 0, 0, 0.5);
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-blend-mode: darken;
+    
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
 `;
+
