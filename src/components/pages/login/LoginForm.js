d9e297e29735f10d40fdcb5036ea16eab1c70a07
Margotte83 <marjorie.ngoupende@gmail.com>
2023-05-12 15:22:59 +0200

    ♻️ F03: LOGIN > loginForm > create <Input/> & add styling

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { BsPersonCircle } from "react-icons/bs";
 import { IoChevronForward } from "react-icons/io5";
+import Input from "./Input";
 
 // import icons from "react-icons";
 export default function LoginForm() {
@@ -26,19 +27,14 @@ export default function LoginForm() {
         <hr />
         <h2>Connectez-vous</h2>
       </div>
-
+      <Input
+        username={value}
+        onChange={handleChange}
+        placeholder="Entrez votre prénom"
+        Icon={<BsPersonCircle className="icon" />}
+        required
+      />
       <div>
-        <div className="input-with-icon">
-          <BsPersonCircle className="icon" />
-          <input
-            type="text"
-            placeholder="Entrez votre prénom"
-            required
-            username={value}
-            onChange={handleChange}
-          />
-        </div>
-
         <button className="button-with-icon">
           <span>Accéder à mon espace</span>
           <IoChevronForward className="icon" />
@@ -68,32 +64,8 @@ const LoginFormStyed = styled.form`
   }
 
   hr {
-    border: 1.5px solid orange;
+    border: 1.5px solid red;
     margin-bottom: 40px;
-  }
-
-  .input-with-icon {
-    background-color: white;
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    padding: 18px 24px;
-    margin: 18px 0;
-    .icon {
-      font-size: 15px;
-      margin-right: 8px;
-      color: gray;
-    }
-    input {
-      border: none;
-      font-size: 15px;
-      color: black;
-    }
-
-    &::placeholder {
-      color: lightgray;
-      background-color: white;
-    }
   }
   .button-with-icon {
     width: 100%;
