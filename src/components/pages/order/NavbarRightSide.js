b607e435b6e549353dcfeceb7329ff70b8708357
Margotte83 <marjorie.ngoupende@gmail.com>
2023-05-30 10:04:09 +0200

    💄 F04: ORDER > Navbar > create profile

--- src/components/pages/order/NavbarRightSide.js
+++ src/components/pages/order/NavbarRightSide.js
@@ -1,16 +1,22 @@
-import { Link } from "react-router-dom";
 import styled from "styled-components";
+import Profile from "./profile.js";
 
 export default function NavbarRightSide({ username }) {
   return (
-    <NavbarRightSideStyled>
-      <h1>Bonjour {username}</h1>
-      <Link to="/">
-        <button>Déconnexion</button>
-      </Link>
+    <NavbarRightSideStyled className="right-side">
+      {/* <div className="admin-button">Admin button</div> */}
+      <Profile username={username} />
     </NavbarRightSideStyled>
   );
 }
 const NavbarRightSideStyled = styled.div`
-  background: pink;
+  display: flex;
+  align-items: center;
+  padding-right: 50px;
+  /* .admin-button {
+    background: lightblue;
+  }*/
+  .profile {
+    background: yellow;
+  }
 `;
