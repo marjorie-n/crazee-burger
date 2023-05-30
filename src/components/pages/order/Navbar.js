eee5693873fc13b009a10dd7dba8410a0969cda9
Margotte83 <marjorie.ngoupende@gmail.com>
2023-05-30 10:37:33 +0200

    ✨ F04: ORDER > Navbar > trigger reload on click

--- src/components/pages/order/Navbar.js
+++ src/components/pages/order/Navbar.js
@@ -3,11 +3,12 @@ import NavbarRightSide from "./NavbarRightSide.js";
 import NavbarLeftSide from "./NavbarLeftSide.js";
 import Logo from "../../reusable-UI/Logo.js";
 import { theme } from "../../../theme/index.js";
+import { refreshPage } from "../../../utils/Window.js";
 
 export default function Navbar({ username }) {
   return (
     <NavbarStyled>
-      <Logo />
+      <Logo className="logo-order-page" onClick={refreshPage} />
       <NavbarLeftSide />
       <NavbarRightSide username={username} />
     </NavbarStyled>
@@ -19,9 +20,23 @@ const NavbarStyled = styled.nav`
   display: flex;
   justify-content: space-between;
   // align-items: center;
-  padding: 0 20px;  
+  padding: 0 20px;
   border-top-left-radius: ${theme.borderRadius.extraRound};
   border-top-right-radius: ${theme.borderRadius.extraRound};
 
+  background-color: ${theme.colors.white};
+  height: 10vh;
+  display: flex;
+  justify-content: space-between;
+  // align-items: center;
+  padding: 0 20px;
+  border-top-left-radius: ${theme.borderRadius.extraRound};
+  border-top-right-radius: ${theme.borderRadius.extraRound};
+  .logo-order-page {
+    cursor: pointer;
+  }
 
+  .logo-order-page {
+    cursor: pointer;
+  }
 `;
