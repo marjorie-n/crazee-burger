4c364b9e94a0ab082ddeba614661a297592a4957
Margotte83 <marjorie.ngoupende@gmail.com>
2023-06-02 09:51:07 +0200

    🚧 F05: ORDER > MAIN> add grid layout

--- src/components/pages/order/Main.js
+++ src/components/pages/order/Main.js
@@ -2,12 +2,26 @@ import styled from "styled-components";
 import { theme } from "../../../theme/index.js";
 
 export default function Main() {
-  return <MainStyled></MainStyled>;
+  return (
+    <MainStyled>
+      <div className="basket">Basket</div>
+      <div className="menu">Menu</div>
+    </MainStyled>
+  );
 }
 const MainStyled = styled.div`
   background: ${theme.colors.background_white};
   flex: 1;
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
-  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset ; 
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  display: grid;
+  grid-template-columns: 25% 1fr;
+  .basket {
+    background: pink;
+  }
+  .menu {
+    background: purple;
+  }
+
 `;
