d7e6838d20c3593ca1d95ee75789198fa9d086bf
Margotte83 <marjorie.ngoupende@gmail.com>
2023-06-02 10:44:03 +0200

    💡 F05: ORDER > MAIN > comment out Basket for later

--- src/components/pages/order/Main/Main.js
+++ src/components/pages/order/Main/Main.js
@@ -5,7 +5,7 @@ import Menu from "./Menu.js";
 export default function Main() {
   return (
     <MainStyled>
-      <div className="basket">Basket</div>
+      {/* <div className="basket">Basket</div> */}
       <Menu />
     </MainStyled>
   );
@@ -16,10 +16,11 @@ const MainStyled = styled.div`
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-  display: grid;
-  grid-template-columns: 25% 1fr;
-  .basket {
-    background: pink;
-  }
+  display: grid;  
+  // add 25% to the grid-template-columns for basket
+  grid-template-columns: 1fr;
+  // .basket {
+  //   background: pink;
+  // }
   
 `;
