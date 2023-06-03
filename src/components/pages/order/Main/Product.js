79584df9352ff1241bad3d637754501cdcd00655
Margotte83 <marjorie.ngoupende@gmail.com>
2023-06-03 17:00:09 +0200

    💄 F05: ORDER > Product > add image layout

--- src/components/pages/order/Main/Product.js
+++ src/components/pages/order/Main/Product.js
@@ -7,8 +7,8 @@ export default function Product({ imageSource, title, price }) {
         <img src={imageSource} alt={title} />
       </div>
       <div className="info-text">
-        <div className="description">
-          <div className="title">{title}</div>
+      <div className="title">{title}</div>
+        <div className="description"></div>
           <div className="price">{price}</div>
           <button className="add-button">Ajouter</button>
         </div>
@@ -19,16 +19,22 @@ export default function Product({ imageSource, title, price }) {
 const ProductStyled = styled.div`
   border: 1px solid black;
   background: red;
-  width: 240px;
-  height: 330px;
+  width: 200px;
+  height: 300px;
+  display: grid;
+  grid-template-rows: 65% 1fr;
+  padding: 20px;
+  padding-bottom: 10px;
 
   .image {
     border: 1px solid black;
-    width: 100px;
+    width: 100%;
     height: auto;
+    margin-top: 50px;
     img {
       width: 100%;
       height: 100%;
+      object-fit: contain;
     }
   }
   .description {
