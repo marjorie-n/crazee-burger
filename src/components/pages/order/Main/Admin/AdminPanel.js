f143ecd5b26870e0225ae0c568edf1303d0a2fa1
Margotte83 <marjorie.ngoupende@gmail.com>
2023-06-20 16:15:54 +0200

    💄 F07: THEME > add shadows

--- src/components/pages/order/Main/Admin/AdminPanel.js
+++ src/components/pages/order/Main/Admin/AdminPanel.js
@@ -22,5 +22,5 @@ const AdminPanelStyled = styled.div`
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   background: ${theme.colors.white};
   border-top: 1px solid ${theme.colors.greyLight};
-  box-shadow: 0px -6px 8px -2px rgba(0, 0, 0, 0.1);
+  box-shadow: ${theme.shadows.subtle};
 `;
