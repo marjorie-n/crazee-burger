bae989512642279e7ddfa6b454702a3a26785b2f
Margotte83 <marjorie.ngoupende@gmail.com>
2023-06-22 11:27:49 +0200

    🚧 F08: ADMIN > AddForm> Add inputs layout

--- src/components/pages/order/Main/Admin/AddForm.js
+++ src/components/pages/order/Main/Admin/AddForm.js
@@ -1,3 +1,38 @@
+import styled from "styled-components";
+
 export default function AddForm() {
-  return <div>AddForm</div>;
+  return (
+    <AddFormStyled>
+      <div className="image-preview">Image</div>
+      <div className="input-fields">
+        <input type="text" placeholder="Nom" className="text" />
+        <input type="text" placeholder="Image URL" />
+        <input type="text" placeholder="Prix" />
+      </div>
+      <button className="submit-button">Submit</button>
+    </AddFormStyled>
+  );
 }
+const AddFormStyled = styled.form`
+  border: 2px solid black;
+  display: grid;
+  // grid-template-columns: 1fr 3fr;
+  // grid-template-rows: repeat(4, 1fr);
+  grid-template: repeat(4, 1fr) / 1fr 3fr;
+  height: 100%;
+  width: 70%;
+  .image-preview {
+    background: blue;
+    grid-area: 1/1 / 4/2;
+  }
+  .input-fields {
+    background: red;
+    grid-area: 1/2 / 4/3;
+    display: grid;
+  }
+  .submit-button {
+    background: green;
+    grid-area: 4/-2 / 5/-1;
+    width: 50%;
+  }
+`;
--- src/components/pages/order/Main/Admin/AdminPanel.js
+++ src/components/pages/order/Main/Admin/AdminPanel.js
@@ -20,4 +20,5 @@ const AdminPanelStyled = styled.div`
   background: ${theme.colors.white};
   border-top: 1px solid ${theme.colors.greyLight};
   box-shadow: ${theme.shadows.subtle};
+  padding: 30px 5%;
 `;
