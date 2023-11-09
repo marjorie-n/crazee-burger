1e29267b392c4f4e7ae5cf9f1e15306c98416bbe
Margotte83 <marjorie.ngoupende@gmail.com>
2023-11-09 11:49:58 +0100

    ♻️  F10: BASKET > add Hook useDisplayMessage

---
+++ src/components/pages/order/Main/Basket/BasketHead.js
@@ -0,0 +1,16 @@
+import { theme } from '@chakra-ui/react';
+import React from 'react'
+import styled from 'styled-components';
+
+export default function BasketHead() {
+    return (
+        < BasketHeadStyled>
+        </BasketHeadStyled>
+    )
+}
+const BasketHeadStyled = styled.div`
+height: 70px;
+background: ${theme.colors.background_dark};
+
+  
+`;
